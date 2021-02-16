f54a9fa3b1601ee667e139ff95085946c0593a31
Fantine Huot <fantine.huot@gmail.com>
2021-02-16 14:59:31 +0100

    Add machine learning configuration

---
+++ config/cnn2d_modular.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+export MODULE_ARGS=" \
+  --model=CNN2DModular \
+  --height=512 \
+  --width=128 \
+  --channels=1 \
+  --tfrecord_height=712 \
+  --tfrecord_width=196 \
+  --num_epochs=10 \
+"
